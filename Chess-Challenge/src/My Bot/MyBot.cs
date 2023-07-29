d7d3b8ca96d050224dc268106c19b35a999c9857
moraxno <moraxno@DESKTOP>
2023-07-29 17:09:57 +0200

    More precise evaluation. Lines are now returned. History is part of analysis.
    All Tests run.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,59 +1,82 @@
 ﻿using ChessChallenge.API;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using static System.Formats.Asn1.AsnWriter;
+using System.Linq;
+using System.Reflection.Emit;
 
+public class Evaluation
+{
+    public int score;
+    public List<Move> line;
+
+    public Evaluation(int score, List<Move> line)
+    {
+        this.score = score;
+        this.line = line;
+    }
+}
 public class MyBot : IChessBot
 {
-    int depth = 0;
+    uint depth = 0;
     int seed = 0;
 
-    // Piece values: null, pawn, knight, bishop, rook, queen, king
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    Evaluation lastEval;
 
-    int softInfinity = 1_000_000;
+    // Piece values:      null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = {    0,  100,    300,    300,  500,   900, 1000 };
+    int[] maxActivity = {    0,    0,      8,     13,   14,    27,    8 };
+
+    int softInfinity = 1_234_567;
 
     Dictionary<ulong, int> cache = new();
 
-    public MyBot(int depth = 2, int seed = 0)
+    public MyBot(uint searchDepth = 3, int seed = 0)
     {
-        this.depth = depth;
+        this.depth = searchDepth;
         this.seed = seed;
+        this.lastEval = new Evaluation(-987654, new());
     }
 
     public bool IsDrawnGame(Board board)
     {
-        return (board.IsInsufficientMaterial() || board.IsRepeatedPosition() || board.FiftyMoveCounter >= 50 || board.GetLegalMoves().Length == 0 && !board.IsInCheckmate());
+        return (board.IsInsufficientMaterial() || board.GameRepetitionHistory.Count(z => z == board.ZobristKey) >= 2 || board.FiftyMoveCounter >= 100 || (board.GetLegalMoves().Length == 0 && !board.IsInCheckmate()));
     }
 
-    public int EvaluateForWhite(Board board, Timer timer)
+    public int EvaluateForWhite(Board board, Timer timer, Stack<ulong> pseudoHistory)
     {
         int score = 0;
+
+        List<ulong> fullHistory = new();
+        fullHistory.AddRange(board.GameRepetitionHistory);
+        fullHistory.AddRange(pseudoHistory);
+
+        if (IsDrawnGame(board) || fullHistory.Count(z => z == board.ZobristKey) >= 3)
+        {
+            return 0;
+        }
 
         if (cache.TryGetValue(board.ZobristKey, out score))
         {
             return score;
         }
 
-        if (IsDrawnGame(board))
-        {
-            return 0;
-        }
-
         if (board.IsInCheckmate())
         {
             return board.IsWhiteToMove ? -softInfinity : softInfinity;
         }
-        
+
         if (board.IsInCheck())
         {
-            score += board.IsWhiteToMove ? -25 : 25;
+            score += board.IsWhiteToMove ? -50 : 50;
         }
 
-        // score = 5 * Math.Max(0, board.FiftyMoveCounter - 40);
+        score -= Math.Sign(score) * board.FiftyMoveCounter;
+
+        var allMoves = board.GetLegalMoves();
 
         foreach (PieceList pieceList in board.GetAllPieceLists())
         {
@@ -63,7 +86,22 @@ public class MyBot : IChessBot
                 int value = pieceValues[(int)piece.PieceType];
 
                 int centrality = 0; // (int)(70.0 - 10.0 * (Math.Abs((float)piece.Square.Rank - 3.5) + Math.Abs((float)piece.Square.File - 3.5)));
-                score += cofactor * (value + centrality);
+                int activity = 0;
+                int kingSafety = 0;
+
+                if (piece.PieceType == PieceType.King)
+                {
+                    kingSafety += Math.Abs(piece.Square.File * 10 - 35) * 5;
+                } else if (piece.PieceType != PieceType.Pawn) 
+                {
+                    activity = 100 * allMoves.Count(m => m.StartSquare == piece.Square) / maxActivity[(int)piece.PieceType];
+                } else
+                {
+                    var targetRank = piece.IsWhite ? 7 : 0;
+                    activity -= 10 * Math.Abs(targetRank - piece.Square.Rank);
+                }
+
+                score += cofactor * (value + centrality + activity);
 
                 //if (piece.IsPawn)
                 //{
@@ -73,36 +111,63 @@ public class MyBot : IChessBot
             }
         }
 
+        int capturePower = allMoves.Count(move => move.IsCapture);
+
+        score += board.HasKingsideCastleRight(true) ? 25 : 0;
+        score += board.HasQueensideCastleRight(true) ? 25 : 0;
+        score -= board.HasKingsideCastleRight(false) ? 25 : 0;
+        score -= board.HasQueensideCastleRight(false) ? 25 : 0;
+
+        if (board.IsWhiteToMove)
+        {
+            score += capturePower;
+        } else
+        {
+            score -= capturePower;
+        }
+
         cache[board.ZobristKey] = score;
         return score;
     }
 
-    public int NegaMax(Board board, Timer timer, bool maximizeForWhite, int depth = 0)
+    public Evaluation NegaMax(Board board, Timer timer, bool maximizeForWhite, Stack<ulong> pseudoHistory, uint depth = 0)
     {
+        List<Move> bestLine = new List<Move>();
+
         // Debug.WriteLine($"Evaluating the board: {board.GetFenString()}");
         if ( depth == 0 || IsDrawnGame(board) || board.IsInCheckmate())
         {
-            return (maximizeForWhite ? 1 : -1 ) * EvaluateForWhite(board, timer);
+            return new Evaluation((maximizeForWhite ? 1 : -1) * EvaluateForWhite(board, timer, pseudoHistory), bestLine);
         }
 
         int bestScore = -softInfinity;
-        int score = -softInfinity;
+        List<Move> bestMoves = new();
+        Move bestMove = new();
         foreach (Move move in board.GetLegalMoves())
         {
             board.MakeMove(move);
-            score = -NegaMax(board, timer, !maximizeForWhite, depth - 1);
+            pseudoHistory.Push(board.ZobristKey);
+            var eval = NegaMax(board, timer, !maximizeForWhite, pseudoHistory, depth - 1);
+            eval.score *= -1;
+            eval.score -= pseudoHistory.Count; // miniscul punishment for deeper searches
             board.UndoMove(move);
+            pseudoHistory.Pop();
 
-            //Debug.WriteLine($"{move.MovePieceType}:{move} > {score}");
-
-            if (score > bestScore)
+            if (eval.score > bestScore)
             {
                 // Debug.WriteLine($"new Best from {bestScore} to {score}.");
-                bestScore = score;
+                bestScore = eval.score;
+                bestMove = move;
+                bestMoves = eval.line;
             }
         }
 
-        return bestScore;
+        bestLine.Add(bestMove);
+        bestLine.AddRange(bestMoves);
+
+        var finalEval = new Evaluation(bestScore, bestLine);
+        this.lastEval = finalEval;
+        return finalEval;
     }
     public Move Think(Board board, Timer timer)
     {
@@ -110,29 +175,46 @@ public class MyBot : IChessBot
         Random rng = new(seed);
         // Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
 
-        int bestScore = int.MinValue;
+        // int bestScore = int.MinValue;
         bool wePlayWhite = board.IsWhiteToMove;
 
-        List<Move> bestMoves = new();
+        uint bonusDepth = 0;
+        int dt = 0;
+        int dt2 = 0;
+        Evaluation eval;
+        eval = NegaMax(board, timer, wePlayWhite, new Stack<ulong>(), this.depth + bonusDepth);
+        //do
+        //{
+        //    dt = timer.MillisecondsRemaining;
+        //    eval = NegaMax(board, timer, wePlayWhite, this.depth + bonusDepth);
+        //    dt2 = timer.MillisecondsRemaining;
+        //    Console.WriteLine($"{eval.score} @ {this.depth + bonusDepth}");
+        //    bonusDepth += 1;
+
+        //} while (dt - dt2 < 250 && eval.score < softInfinity && timer.MillisecondsElapsedThisTurn < 1500 && timer.MillisecondsRemaining > 10_000);
+
+        return eval.line[0];
+
+        //List<Move> bestMoves = new();
 
 
-        foreach (Move move in allMoves)
-        {
-            board.MakeMove(move);
-            int eval = NegaMax(board, timer, wePlayWhite, this.depth);
-            board.UndoMove(move);
+        //foreach (Move move in allMoves)
+        //{
+        //    board.MakeMove(move);
+        //    int eval = NegaMax(board, timer, wePlayWhite, this.depth).score;
+        //    board.UndoMove(move);
 
-            if ( eval > bestScore)
-            {
-                bestScore = eval;
-                bestMoves.Clear();
-                bestMoves.Add(move);
-            } else if (eval == bestScore)
-            {
-                bestMoves.Add(move);
-            }
-        }
+        //    if ( eval > bestScore)
+        //    {
+        //        bestScore = eval;
+        //        bestMoves.Clear();
+        //        bestMoves.Add(move);
+        //    } else if (eval == bestScore)
+        //    {
+        //        bestMoves.Add(move);
+        //    }
+        //}
 
-        return bestMoves[rng.Next(bestMoves.Count)];
+        //return bestMoves[rng.Next(bestMoves.Count)];
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -2,6 +2,7 @@ using ChessChallenge.API;
 using ChessChallenge.Example;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Engine = ChessChallenge.Chess;
 
 namespace Tests
@@ -13,6 +14,41 @@ namespace Tests
             return new ChessChallenge.API.Timer(millis);
         }
 
+        public static IEnumerable<object[]> MakeConfiguration(string[] fenPositions, uint minDepth, uint maxDepth, bool flipBoard=true, bool loopSeeds=true)
+        {
+            //List<int> seeds = new ();
+            //if (loopSeeds)
+            //{
+            //    seeds.AddRange(new int[] { 0, 1, 42, 420, 1337, 0xCAFE, 0xAFFE, 0xBEEF });
+            //} else
+            //{
+            //    seeds.Add(0);
+            //}
+
+            foreach (var position in fenPositions)
+            {
+                List<string> fenViews = new List<string>();
+                fenViews.Add(position);
+                if (flipBoard)
+                {
+                    fenViews.Add(Engine.FenUtility.FlipFen(position));
+                }
+
+                for (uint depth = minDepth; depth <= maxDepth; depth++)
+                {
+                    foreach (var fen in fenViews)
+                    {
+                        //foreach (var seed in seeds)
+                        //{
+                        yield return new object[] { fen, depth }; //, seed };
+                        //}   
+                    }
+                }
+            }
+        }
+
+        
+
         [Theory]
         [InlineData("r1b1k1nr/ppppqppp/2n5/2b1P3/8/2N2N2/PPP1PPPP/R1BQKB1R w KQkq - 0 1")]
         [InlineData("2kr3r/p1ppqp2/bpn2p1p/8/1P4P1/2N2N2/P1PBPK1P/1R1Q1B1R b Hk - 0 1")]
@@ -23,19 +59,30 @@ namespace Tests
 
             string flippedPosition = Engine.FenUtility.FlipFen(fenPosition);
 
-            int fenEval = bot.EvaluateForWhite(Board.CreateBoardFromFEN(fenPosition), EasyTimer());
-            int flipEval = bot.EvaluateForWhite(Board.CreateBoardFromFEN(flippedPosition), EasyTimer());
+            int fenEval = bot.EvaluateForWhite(Board.CreateBoardFromFEN(fenPosition), EasyTimer(), new Stack<ulong>());
+            int flipEval = bot.EvaluateForWhite(Board.CreateBoardFromFEN(flippedPosition), EasyTimer(), new Stack<ulong>());
 
             Assert.Equal(fenEval, -flipEval);
         }
 
+        [Theory]
+        [InlineData("7Q/8/8/8/4k3/8/8/K7 w - - 0 1", "7R/8/8/8/4k3/8/8/K7 w - - 0 1")]
+        public void EvaluationForQueenIsBetterThanRook(String fenPositionQ, String fenPositionR)
+        {
+            MyBot bot = new();
+
+            int evalQ = bot.EvaluateForWhite(Board.CreateBoardFromFEN(fenPositionQ), EasyTimer(), new Stack<ulong>());
+            int evalR = bot.EvaluateForWhite(Board.CreateBoardFromFEN(fenPositionR), EasyTimer(), new Stack<ulong>());
+
+            Assert.True(evalQ > evalR);
+        }
+
 
         [Theory]
-        [InlineData("k7/7P/8/8/8/8/8/K7 w - - 0 1", 0)]
-        [InlineData("k7/7P/8/8/8/8/8/K7 w - - 0 1", 1)]
-        [InlineData("k7/7P/8/8/8/8/8/K7 w - - 0 1", 2)]
-        [InlineData("k7/7P/8/8/8/8/8/K7 w - - 0 1", 3)]
-        public void PromotionFavorsQueens(String fenPosition, int depth)
+        [InlineData("8/7P/8/8/4k3/8/8/K7 w - - 0 1", 1)]
+        [InlineData("8/7P/8/8/4k3/8/8/K7 w - - 0 1", 2)]
+        [InlineData("8/7P/8/8/4k3/8/8/K7 w - - 0 1", 3)]
+        public void PromotionFavorsQueens(String fenPosition, uint depth)
         {
             string[] positions = { fenPosition, Engine.FenUtility.FlipFen(fenPosition) };
             MyBot bot = new MyBot(depth);
@@ -50,26 +97,20 @@ namespace Tests
             }
         }
 
+        public static IEnumerable<object[]> checkMateInOneData = 
+            MakeConfiguration(new string[] { "k7/7R/1K6/8/8/8/8/8 w - - 0 1" }, 1, 5);
         [Theory]
-        [InlineData("k7/7R/1K6/8/8/8/8/8 w - - 0 1", 0)]
-        [InlineData("k7/7R/1K6/8/8/8/8/8 w - - 0 1", 1)]
-        [InlineData("k7/7R/1K6/8/8/8/8/8 w - - 0 1", 2)]
-        [InlineData("k7/7R/1K6/8/8/8/8/8 w - - 0 1", 3)]
-        public void CheckMatesInOneAreTaken(String fenPosition, int depth)
+        [MemberData(nameof(checkMateInOneData))]
+        public void CheckMatesInOneAreTaken(String fenPosition, uint depth)
         {
-            string[] positions = { fenPosition, Engine.FenUtility.FlipFen(fenPosition) };
             MyBot bot = new MyBot(depth);
+            Board b = Board.CreateBoardFromFEN(fenPosition);
 
-            foreach (String fen in positions)
-            {
-                Board b = Board.CreateBoardFromFEN(fenPosition);
+            Move move = bot.Think(b, EasyTimer());
+            b.MakeMove(move);
 
-                Move move = bot.Think(b, EasyTimer());
-                b.MakeMove(move);
-
-                Assert.Equal(PieceType.Rook, move.MovePieceType);
-                Assert.True(b.IsInCheckmate());
-            }
+            Assert.Equal(PieceType.Rook, move.MovePieceType);
+            Assert.True(b.IsInCheckmate());
         }
 
         [Theory]
@@ -83,7 +124,7 @@ namespace Tests
         [InlineData("3k4/R7/2K5/8/8/8/8/8 b - - 0 1", 3, 420)]
         [InlineData("3k4/R7/2K5/8/8/8/8/8 b - - 0 1", 2, -174)]
         [InlineData("3k4/R7/2K5/8/8/8/8/8 b - - 0 1", 3, -174)]
-        public void CheckMatesInOneAreEscaped(String fenPosition, int depth, int seed=0)
+        public void CheckMatesInOneAreEscaped(String fenPosition, uint depth, int seed=0)
         {
             string[] positions = { fenPosition, Engine.FenUtility.FlipFen(fenPosition) };
 
@@ -104,7 +145,7 @@ namespace Tests
 
         [Theory]
         [InlineData("8/8/8/8/3k4/8/8/K6Q w - - 0 1", 3)]
-        public void DoesNotBlunderAQueen(String fenPosition, int depth)
+        public void DoesNotBlunderAQueen(String fenPosition, uint depth)
         {
             string[] positions = { fenPosition, Engine.FenUtility.FlipFen(fenPosition) };
             MyBot bot = new MyBot(depth);
@@ -130,7 +171,7 @@ namespace Tests
 
         [Theory]
         [InlineData("k3r3/8/8/1N6/3p4/8/8/K7 w - - 0 1", 4)]
-        public void FindsFork(String fenPosition, int depth)
+        public void FindsFork(String fenPosition, uint depth)
         {
             string[] positions = { fenPosition, Engine.FenUtility.FlipFen(fenPosition) };
             MyBot bot = new MyBot(depth);
@@ -161,6 +202,13 @@ namespace Tests
                 Assert.Equal(PieceType.Knight, move.MovePieceType);
                 Assert.Equal(PieceType.Rook, move.CapturePieceType);
             }
+        }
+
+        [Theory]
+        [InlineData("6k1 / 7p / b6P / p7 / 8 / 8 / 4r1p1 / 2K5 w - -0 53", 1)]
+        public void DoesNotPerformIllegalMove(string fenPosition, uint depth)
+        {
+
         }
     }
 }
